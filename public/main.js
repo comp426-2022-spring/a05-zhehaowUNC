6839844e06e8a06a0a1d22f97112a915f8e84b55
zhehaowUNC <69946051+zhehaowUNC@users.noreply.github.com>
2022-04-27 15:35:15 -0400

    Update main.js

--- public/main.js
+++ public/main.js
@@ -52,8 +52,8 @@ function flipCoins() {
             // Graphically display all results in the details table.
             var detailsTableBody = document.getElementById("realresults");
             detailsTableBody.innerHTML = result.raw;
-            document.getElementById("heads").innerHTML = result.summary.heads == undefined ? 0 : result.summary.heads
-            document.getElementById("tails").innerHTML = result.summary.tails == undefined ? 0 : result.summary.tails
+            document.getElementById("heads").innerHTML = result.summary.heads == undefined ? "heads: " + 0.toString() : "heads: " + result.summary.heads.toString()
+            document.getElementById("tails").innerHTML = result.summary.tails == undefined ? "tails: " + 0.toString() : "tails: " + result.summary.tails.toString()
         })
 }
 
